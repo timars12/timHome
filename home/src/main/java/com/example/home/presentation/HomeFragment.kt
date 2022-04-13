974aa02ef6e839d4a187a7e3c234f0792a4d2e07
ruslantim <ryslan.timkov@israelit.pro>
2022-04-13 19:16:19 +0300

    change text and add gradient to home fragment

--- home/src/main/java/com/example/home/presentation/HomeFragment.kt
+++ home/src/main/java/com/example/home/presentation/HomeFragment.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.home.presentation.home.HomeViewModel
@@ -28,12 +35,35 @@ class HomeFragment : Fragment() {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 MaterialTheme {
+
+                    val brush = Brush.verticalGradient(
+                        colors = listOf(
+                            MaterialTheme.colors.primaryVariant,
+                            Color(0xffffc0cb)
+                        )
+                    )
+
                     Box(
-                        modifier = Modifier.fillMaxSize()
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(brush),
+                        contentAlignment = Alignment.Center
                     ) {
-                        Text(text = "Home Fragment", modifier = Modifier.clickable {
-                            viewModel::logSomething
-                        })
+                        Column {
+                            Text(
+                                text = "Hi! You are in Home fragment.",
+                                modifier = Modifier.clickable { viewModel::logSomething },
+                                textAlign = TextAlign.Center,
+                                color = Color.White,
+                                fontSize = 26.sp
+                            )
+                            Text(
+                                text = "We will implement another features later",
+                                textAlign = TextAlign.Left,
+                                color = Color.White,
+                                fontSize = 14.sp
+                            )
+                        }
                     }
                 }
             }
