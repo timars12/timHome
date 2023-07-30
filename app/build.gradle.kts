1acc3fba87936a89c664b09f35accc862d042fe5
Ruslan Timkov <ruslan.timkov@israelit.pro>
2023-07-30 18:21:46 +0300

    update versionCode & versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     defaultConfig {
         applicationId = "com.timhome.modularizationtest"
 
-        versionCode = 9
-        versionName = "1.1"
+        versionCode = 10
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["androidx.benchmark.suppressErrors"] = "EMULATOR"
