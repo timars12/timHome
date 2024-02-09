50d0af53fd15402eada00b1baeff563cfe5d5bab
Ruslan Timkov <ruslan.timkov@israelit.pro>
2024-02-09 02:12:32 +0200

    test 2 m

--- feature/home/src/main/java/com/example/home/custom/TemperatureBar.kt
+++ feature/home/src/main/java/com/example/home/custom/TemperatureBar.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.home.R
 
-private const val SIZE_WIDTH_IN_DOOR_TEMPERATURE = 0.8f
+private const val SIZE_WIDTH_IN_DOOR_TEMPERATURE = 0.3f
 private const val SIZE_HEIGHT_IN_DOOR_TEMPERATURE = 0.7f
 private const val SIZE_WIDTH_OUT_DOOR_TEMPERATURE = 0.2f
 private const val SIZE_HEIGHT_OUT_DOOR_TEMPERATURE = 0.4f
